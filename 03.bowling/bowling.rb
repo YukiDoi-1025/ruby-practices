c71ddf24bbd8256e2b2d319f4ce861b183d53695
yukidoi <doi_y@vitallead.co.jp>
2025-05-07 09:23:24 +0900

    Use flat_map and sum

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,38 +2,18 @@
 
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.split(',')
-shots = []
-# 数値に変換
-scores.each do |s|
-  if s == 'X' # ストライクの場合
-    shots << 10
-    shots << 0
-  else # それ以外
-    shots << s.to_i
-  end
-end
+shots = ARGV[0].split(',').flat_map { |s| s == 'X' ? [10, 0] : s.to_i }
 
 # フレームごとに分割
 frames = shots.each_slice(2).to_a
 
-# 合計計算
-point = 0
-# ボウリングは必ず10フレームで固定
-10.times do |num|
-  point +=
+point = 10.times.sum do |num| # ボウリングは必ず10フレームで固定
+  frames[num].sum +
     if frames[num][0] == 10 # ストライク
-      if frames [num + 1][0] == 10
-        frames[num].sum + frames[num + 1].sum + frames[num + 2][0]
-      else
-        frames[num].sum + frames[num + 1].sum
-      end
+      frames[num + 1].sum + (frames [num + 1][0] == 10 ? frames[num + 2][0] : 0)
     elsif frames[num].sum == 10 # スペア
-      frames[num].sum + frames[num + 1][0]
-    else
-      frames[num].sum
-    end
+      frames[num + 1][0]
+    end.to_i
 end
 
 puts point
