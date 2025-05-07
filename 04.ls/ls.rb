1650449da9796ceef72e9b22744312826e75fd17
yukidoi <doi_y@vitallead.co.jp>
2025-05-08 08:51:45 +0900

    Change variable name

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,17 +4,20 @@
 
 require 'optparse'
 
+COLUMN_NUMBER = 3
+
 def split_file_names_by_columns(col_num)
   file_names = Dir.glob('*').sort { |a, b| a.delete('.') <=> b.delete('.') }
   row_num = (file_names.size / col_num.to_f).ceil
   Array.new(col_num) { file_names.slice!(0, row_num) }
 end
 
-def display_file(splited_file_names, col_num)
-  max_chara_numbers = splited_file_names.map { |a| a.map(&:size).max }
-  splited_file_names[0].size.times do |row|
+def display_file(split_file_names, col_num)
+  max_filename_sizes = split_file_names.map { |a| a.map(&:size).max }
+  split_file_names[0].size.times do |row|
     col_num.times do |col|
-      print splited_file_names[col][row] ? "#{splited_file_names[col][row].ljust(max_chara_numbers[col])}  " : ''
+      file_name = split_file_names[col][row]
+      print file_name ? "#{file_name.ljust(max_filename_sizes[col])}  " : ''
     end
     puts
   end
@@ -23,6 +26,5 @@ end
 opt = OptionParser.new
 opt.parse(ARGV)
 
-COLUMN_NUMBER = 3
-splited_file_names = split_file_names_by_columns(COLUMN_NUMBER)
-display_file(splited_file_names, COLUMN_NUMBER)
+split_file_names = split_file_names_by_columns(COLUMN_NUMBER)
+display_file(split_file_names, COLUMN_NUMBER)
