07f9b70626bd24e589baa5e7ed4361fdd09ff37c
yukidoi <doi_y@vitallead.co.jp>
2025-05-14 10:29:50 +0900

    Changed bonus calculation to Frame class

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -11,11 +11,22 @@ class Frame
     @third_shot = Shot.new(third_mark)
   end
 
-  def score(frame_count)
-    if frame_count == 9
+  def score
+    if @third_shot
       [@first_shot.score, @second_shot.score, @third_shot.score].sum
     else
       [@first_shot.score, @second_shot.score].sum
     end
+  end
+
+  def bonus(next_frame, after_next_frame)
+    if next_frame
+      if @first_shot.score == 10 # ストライク
+        next_frame.first_shot.score + next_frame.second_shot.score + \
+          (next_frame.first_shot.score == 10 && after_next_frame ? after_next_frame.first_shot.score : 0)
+      elsif score == 10 # スペア
+        next_frame.first_shot.score
+      end
+    end.to_i
   end
 end
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -11,28 +11,22 @@ class Game
     frames = shots.each_slice(2).to_a
 
     if frames[FINAL_FRAME][0] == 'X'
-      frames[FINAL_FRAME][1] = frames[FINAL_FRAME + 1][0]
-      frames[FINAL_FRAME][2] = frames[FINAL_FRAME + 1][0] == 'X' ? frames[FINAL_FRAME + 2][0] : frames[FINAL_FRAME + 1][1]
-    elsif frames[FINAL_FRAME][0].to_i + frames[FINAL_FRAME][1].to_i == 10
-      frames[FINAL_FRAME][2] = frames[FINAL_FRAME + 1][0]
+      (1..2).each do |i|
+        frames[FINAL_FRAME][i] = frames[FINAL_FRAME + 1][0] == 'X' ? frames[FINAL_FRAME + i][0] : frames[FINAL_FRAME + 1][i - 1]
+      end
     end
 
+    frames[FINAL_FRAME][2] = frames[FINAL_FRAME + 1][0] if frames[FINAL_FRAME][0].to_i + frames[FINAL_FRAME][1].to_i == 10
+
     @frames = (0..FINAL_FRAME).map do |frame_count|
-      Frame.new(frames[frame_count][0], frames[frame_count][1], frames[frame_count][2])
+      Frame.new(*frames[frame_count][0..1], frames[frame_count][2])
     end
   end
 
   def score
     (0..FINAL_FRAME).sum do |frame_count|
       current_frame, next_frame, after_next_frame = @frames.slice(frame_count, 3)
-      bonus_score = if next_frame
-                      if current_frame.first_shot.score == 10 # ストライク
-                        next_frame.score(frame_count) + (next_frame.first_shot.score == 10 && after_next_frame ? after_next_frame.first_shot.score : 0)
-                      elsif current_frame.score(frame_count) == 10 # スペア
-                        next_frame.first_shot.score
-                      end
-                    end.to_i
-      current_frame.score(frame_count) + bonus_score
+      current_frame.score + current_frame.bonus(next_frame, after_next_frame)
     end
   end
 end
