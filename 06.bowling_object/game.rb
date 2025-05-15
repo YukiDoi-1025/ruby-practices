869029abc1d5f31e2f84b63c8719e5c940d90f0c
yukidoi <doi_y@vitallead.co.jp>
2025-05-15 09:15:24 +0900

    Change order of assignment

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -10,13 +10,13 @@ class Game
     shots = game.split(',').flat_map { |s| s == 'X' ? %w[X 0] : s }
     frames = shots.each_slice(2).to_a
 
+    frames[FINAL_FRAME][2] = frames[FINAL_FRAME + 1][0] if frames[FINAL_FRAME + 1]
+
     if frames[FINAL_FRAME][0] == 'X'
       (1..2).each do |i|
         frames[FINAL_FRAME][i] = frames[FINAL_FRAME + 1][0] == 'X' ? frames[FINAL_FRAME + i][0] : frames[FINAL_FRAME + 1][i - 1]
       end
     end
-
-    frames[FINAL_FRAME][2] = frames[FINAL_FRAME + 1][0] if frames[FINAL_FRAME][0].to_i + frames[FINAL_FRAME][1].to_i == 10
 
     @frames = (0..FINAL_FRAME).map do |frame_count|
       Frame.new(*frames[frame_count][0..1], frames[frame_count][2])
