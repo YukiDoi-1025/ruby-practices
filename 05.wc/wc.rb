bdb7f234c0a71b8523345655b19ddd06a52b029f
yukidoi <doi_y@vitallead.co.jp>
2025-05-21 15:50:00 +0900

    Support standard input

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -6,18 +6,15 @@ require 'optparse'
 
 OPTION_KEY = { l: :line, w: :word, c: :size }.freeze
 
-def word_count(file_name)
-  File.open(file_name) do |file|
-    file_contents = file.readlines
-    line_number = file_contents.size
-    line_number -= 1 unless file_contents[-1].end_with?("\n", "\r")
-    {
-      line: line_number,
-      word: file_contents.sum { |text| text.split.size },
-      size: file.size,
-      name: file_name
-    }
-  end
+def word_count(contents, file_name = '')
+  line_number = contents.size
+  line_number -= 1 unless contents[-1].end_with?("\n", "\r")
+  {
+    line: line_number,
+    word: contents.sum { |text| text.split.size },
+    size: contents.sum(&:bytesize),
+    name: file_name
+  }
 end
 
 opt = OptionParser.new
@@ -29,18 +26,38 @@ opt.on('-w') { |v| v }
 opt.on('-c') { |v| v }
 opt.parse!(ARGV, into: params)
 
-all_count_results = ARGV.map { |file_name| word_count(file_name) }
+all_count_results = if FileTest.pipe?($stdin)
+                      [word_count($stdin.readlines)]
+                    else
+                      ARGV.map do |file_name|
+                        File.open(file_name) do |file|
+                          file_contents = file.readlines
+                          word_count(file_contents, file_name)
+                        end
+                      end
+                    end
 
 total_count_results = {}
 OPTION_KEY.each_value do |result_key|
   total_count_results[result_key] = all_count_results.sum { |optioned_count_result| optioned_count_result[result_key] }
 end
-total_size_digit = total_count_results.max_by { |a| a[1] }[1].to_s.size
+
+total_size_digit = FileTest.pipe?($stdin) ? 7 : total_count_results.max_by { |a| a[1] }[1].to_s.size
 
 all_count_results.map do |optioned_count_result|
-  OPTION_KEY.each { |option, result_key| print "#{optioned_count_result[result_key].to_s.rjust(total_size_digit)} " if params.key?(option) || params.empty? }
+  OPTION_KEY.each do |option, result_key|
+    if params.key?(option) || params.empty?
+      if FileTest.pipe?($stdin) && params.size == 1
+        print optioned_count_result[result_key]
+      else
+        print "#{optioned_count_result[result_key].to_s.rjust(total_size_digit)} "
+      end
+    end
+  end
   puts optioned_count_result[:name]
 end
 
-OPTION_KEY.each { |option, result_key| print "#{total_count_results[result_key].to_s.rjust(total_size_digit)} " if params.key?(option) || params.empty? }
-puts 'total'
+if all_count_results.size > 1
+  OPTION_KEY.each { |option, result_key| print "#{total_count_results[result_key].to_s.rjust(total_size_digit)} " if params.key?(option) || params.empty? }
+  puts 'total'
+end
