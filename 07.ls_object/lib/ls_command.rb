66d7b54518f48941b0dd11ce84078f0eb563415c
yukidoi <doi_y@vitallead.co.jp>
2025-05-22 10:27:16 +0900

    Create object-oriented ls command

--- 07.ls_object/lib/ls_command.rb
+++ 07.ls_object/lib/ls_command.rb
@@ -1,31 +1,38 @@
+# frozen_string_literal: true
+
 require_relative 'file_attribute'
 
 class LsCommand
   def initialize(option, column_number)
     @option = option
-    file_names = @option.has_key?(:a) ? Dir.entries('.').sort : Dir.glob('*')
-    file_names.reverse! if @option.has_key?(:r)
-    @file_attributes = file_names.map { |file_name| FileAttribute.new(file_name) }
+    @file_names = @option.key?(:a) ? Dir.entries('.').sort : Dir.glob('*')
+    @file_names.reverse! if @option.key?(:r)
+    @file_attributes = @file_names.map { |file_name| FileAttribute.new(file_name) }
     @column_number = column_number
     @total_block = @file_attributes.map(&:block_num).sum
-    @muximum_link_digit = Math.log10(@file_attributes.map(&:hard_link).max).floor + 1
-    @muximum_file_size_digit = Math.log10(@file_attributes.map(&:file_size).max).floor + 1
   end
 
   def display_files
-    # lオプションの有無での表示切替機能を実装する
-    puts "total #{@total_block}"
-    @file_attributes.each do |file_attribute|
-      puts file_attribute.output_file_infomation(@muximum_link_digit, @muximum_file_size_digit).join(' ')
+    if @option.key?(:l)
+      muximum_hard_link_digit = @file_attributes.map { |file_attribute| file_attribute.hard_link.to_s.length }.max
+      muximum_file_size_digit = @file_attributes.map { |file_attribute| file_attribute.file_size.to_s.length }.max
+      puts "total #{@total_block}"
+      @file_attributes.each do |file_attribute|
+        puts file_attribute.output_file_infomation(muximum_hard_link_digit, muximum_file_size_digit).join(' ')
+      end
+    else
+      display_files_split(split_file_names_by_columns)
     end
   end
 
-  def split_file_names_by_columns(col_num, show_reverse_order)
-  file_names = show_reverse_order ? Dir.glob('*').reverse : Dir.glob('*')
-  row_num = (file_names.size / col_num.to_f).ceil
-  Array.new(col_num) { file_names.slice!(0, row_num) }
+  private
+
+  def split_file_names_by_columns
+    row_num = (@file_names.size / @column_number.to_f).ceil
+    Array.new(@column_number) { @file_names.slice!(0, row_num) }
   end
-  def display_file(split_file_names)
+
+  def display_files_split(split_file_names)
     max_filename_sizes = split_file_names.map { |a| a.map(&:size).max }
     split_file_names[0].size.times do |row|
       split_file_names.size.times do |col|
--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# frozen_string_literal: true
+
 require 'optparse'
 require_relative './lib/ls_command'
 
@@ -7,16 +9,13 @@ COLUMN_NUMBER = 3
 
 opt = OptionParser.new
 
-opt.on('-a [VAL]') { |v| v }
-opt.on('-r [VAL]') { |v| v }
-opt.on('-l [VAL]') { |v| v }
-opt.parse(ARGV)
+option = {}
 
-divided_option = {}
-ARGV[0][1..].to_s.chars.each { |char| divided_option[char.to_sym] = true}
+opt.on('-a') { |v| v }
+opt.on('-r') { |v| v }
+opt.on('-l') { |v| v }
+opt.parse!(ARGV, into: option)
 
-p divided_option
-
-ls = LsCommand.new(divided_option, COLUMN_NUMBER)
+ls = LsCommand.new(option, COLUMN_NUMBER)
 
 ls.display_files
